94364937ca6406d28870d1cf57bdb47dc012bd93
kobejean <s1292010@u-aizu.ac.jp>
2023-04-01 06:42:21 +0900

    yuki's updates

---
+++ notebooks/yuki/NeRF/conv_json_to_ScanNerf-NSVF/convert_scannerf.sh
@@ -0,0 +1,14 @@
+# TRAIN_JSON="train_all_500.json"
+VAL_JSON="val_all.json"
+TEST_JSON="test_all.json"
+SCANNERF_DIR="/home/ccl/Datasets/NeRF/ScanNerf"
+
+for SUFFIX in all_25_horizontal all_10_horizontal; do
+  TRAIN_JSON="train_$SUFFIX.json"
+  OUT_DIR="../ScanNerf-NSVF/$SUFFIX"
+  for SCENE_PATH in $SCANNERF_DIR/*; do
+    SCENE=`basename $SCENE_PATH`
+    echo $SCENE
+    python scannerf2nsvf.py "$SCANNERF_DIR/$SCENE" "$OUT_DIR/$SCENE" --train_json=$TRAIN_JSON --val_json=$VAL_JSON --test_json=$TEST_JSON
+  done
+done
